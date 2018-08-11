6532119dee9462db67ee2ef58fdae2d861cb751b
Yevgen Polukov <yevgen.polukov@paycash.onmicrosoft.com>
2018-08-11 11:04:11 +0300

    add exception handling & simple error page.

---
+++ src/main/java/com/levik/searchclient/controller/ExceptionHandlingController.java
@@ -0,0 +1,24 @@
+package com.levik.searchclient.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ExceptionHandlingController {
+
+    private static final Logger log = LoggerFactory.getLogger(ExceptionHandlingController.class);
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+        log.error("Request: " + req.getRequestURL() + " raised " + ex);
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("error");
+        return mav;
+    }
+}
